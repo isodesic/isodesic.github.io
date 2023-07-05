3b4c7a5590133a013602d88c14335dab657dab7c
isodesic <137956968+isodesic@users.noreply.github.com>
2023-07-05 12:19:16 -0600

    update

--- images/full-width-images/gen_json.js
+++ images/full-width-images/gen_json.js
@@ -3,6 +3,60 @@
 
 const fs = require('fs');
 const path = require('path');
+const probe = require('probe-image-size'); // Requires the 'probe-image-size' package (npm install probe-image-size)
+
+const folderPath = './'; // Replace with the actual folder path
+const jsonFilePath = './images.json'; // Replace with the desired JSON file path
+
+fs.readdir(folderPath, (err, files) => {
+  if (err) {
+    console.error('Error reading directory:', err);
+    return;
+  }
+
+  const imageFiles = files.filter(file => {
+    const fileExtension = path.extname(file).toLowerCase();
+    return fileExtension === '.jpg' || fileExtension === '.jpeg' || fileExtension === '.png';
+  });
+
+  const landscapeImagePaths = [];
+
+  imageFiles.forEach(file => {
+    const filePath = path.join(folderPath, file);
+    const dimensions = probe.sync(fs.readFileSync(filePath));
+    if (dimensions.width > 1279) {
+      if (dimensions.orientation) {
+        if (dimensions.orientation < 5) {
+          console.log(filePath + ': width = ' + dimensions.width + ', height = ' + dimensions.height + ', orientation = ' + dimensions.orientation + '\n');
+          landscapeImagePaths.push(filePath);
+        }
+      } else {
+        if (dimensions.width > dimensions.height) {
+          console.log(filePath + ': width = ' + dimensions.width + ', height = ' + dimensions.height + ', orientation = ' + dimensions.orientation + '\n');
+          landscapeImagePaths.push(filePath);
+        }
+      }
+    }
+  });
+
+  const imagesJson = {
+    images: landscapeImagePaths
+  };
+
+  fs.writeFile(jsonFilePath, JSON.stringify(imagesJson), err => {
+    if (err) {
+      console.error('Error writing JSON file:', err);
+      return;
+    }
+    console.log('JSON file generated successfully.');
+  });
+});
+
+
+
+/*
+const fs = require('fs');
+const path = require('path');
 
 const folderPath = './'; // Replace with the actual folder path
 const jsonFilePath = './images.json'; // Replace with the desired JSON file path
@@ -30,3 +84,4 @@ fs.readdir(folderPath, (err, files) => {
     console.log('JSON file generated successfully.');
   });
 });
+*/
